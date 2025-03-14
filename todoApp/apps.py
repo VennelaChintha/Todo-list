f3a5369f7f7605a166f57519d21091dd4972f49c
VennelaChintha <141574569+VennelaChintha@users.noreply.github.com>
2025-03-14 14:38:31 +0530

    Add files via upload

---
+++ todoApp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TodoAppConfig(AppConfig):
+
+    name = 'todoApp'
---
+++ todoApp/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Todo(models.Model):
+    title = models.CharField(max_length=100)
+    created_at = models.DateTimeField('Created', auto_now_add=True)
+    update_at = models.DateTimeField('Updated', auto_now=True)
+    isCompleted = models.BooleanField(default=False)
+
+    def _str_(self):
+        return self.title
+
+# Create your models here.
